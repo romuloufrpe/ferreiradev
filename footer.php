2b6597c7df8b12c2a935db0e640969c292b5a18f
Administrador <administrador@MacBook-de-Administrador.local>
2017-04-26 21:25:50 -0300

    estilo recuperado

--- footer.php
+++ footer.php
@@ -3,13 +3,13 @@
 <footer class="page-footer teal accent-4">
   <div class="footer-copyright ">
         <div class="container direitos">
-        © 2017 Todos os Direitos Reservados.
+        © <?php echo date('Y');?> Todos os Direitos Reservados.
         </div>
   </div>
 </footer>
 <!--Import jQuery before materialize.js-->
 <script type="text/javascript" src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
-<script type="text/javascript" src="bower_components/materialize/dist/js/materialize.min.js"></script>
+<script type="text/javascript" src="<?php bloginfo('template_directory')?>/bower_components/materialize/dist/js/materialize.min.js"></script>
 <script>
 
 $(".button-collapse").sideNav();
--- header.php
+++ header.php
@@ -2,15 +2,15 @@
 <html>
   <head>
     <meta charset="utf-8">
-    <title>FerreiraDev!</title>
+    <title> <?php bloginfo('name'); ?> </title>
     <!--Import Google Icon Font-->
-      <link href="http://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+      <link href="<?php bloginfo('template_url'); ?>/http://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
       <!--Import materialize.css-->
-      <link type="text/css" rel="stylesheet" href="bower_components/materialize/dist/css/materialize.min.css"  media="screen,projection"/>
-      <link rel="stylesheet" href="bower_components/font-awesome/css/font-awesome.min.css">
+      <link type="text/css" rel="stylesheet" href="<?php bloginfo('template_url'); ?>/bower_components/materialize/dist/css/materialize.min.css"  media="screen,projection"/>
+      <link rel="stylesheet" href="<?php bloginfo('template_url'); ?>/bower_components/font-awesome/css/font-awesome.min.css">
       <!--Let browser know website is optimized for mobile-->
      <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
-     <link rel="stylesheet" href="css/<?php echo $style ?>.css">
+     <link rel="stylesheet" href="<?php bloginfo('template_url'); ?>/css/<?php echo $style ?>.css">
 
   </head>
   <body>
@@ -25,7 +25,11 @@
             <li><a href="portifolio.php">Portfólio</a></li>
             <li><a href="contato.php">Contato</a></li>
             <li><a href="sobre.php">Sobre</a></li>
+            <li><a href="#"><i class="fa fa-facebook" aria-hidden="true"></i></a></li>
+            <li><a href="#"><i class="fa fa-instagram" aria-hidden="true"></i></a></li>
           </ul>
+
+
 
           <ul id="nav-mobile" class="side-nav" style="transform: translateX(-100%);">
             <li><a href="index.php">Início</a></li>
@@ -50,7 +54,7 @@
             </div>
         </div>
         <div class="parallax">
-          <img src="imagens/header-parallax.jpg">
+          <img src="<?php bloginfo('template_url');?>/imagens/header-parallax.jpg">
         </div>
       </div>
     </header>
